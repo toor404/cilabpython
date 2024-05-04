8646923729fc4a1342de4f17bce3f6e1420df2e5
nolastname <43694360+toor404@users.noreply.github.com>
2024-05-04 07:08:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, SMK TI ANNISA!'
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)  # Menjalankan aplikasi di port 3000
